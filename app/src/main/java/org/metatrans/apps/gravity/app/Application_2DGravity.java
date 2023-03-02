45feb896ff19c31880770b51be5a511066c8ce3e
Krasimir Topchiyski <k_topchiyski@yahoo.com>
2023-03-02 22:09:39 +0200

    Adopt changes and fix compilation issue

--- app/src/main/java/org/metatrans/apps/gravity/app/Application_2DGravity.java
+++ app/src/main/java/org/metatrans/apps/gravity/app/Application_2DGravity.java
@@ -1,9 +1,9 @@
 package org.metatrans.apps.gravity.app;
 
 
-import org.metatrans.apps.gravity.app.Application_Gravity;
-import org.metatrans.apps.gravity.cfg.ads.AdsConfigurations_Gravity;
-import org.metatrans.commons.ads.api.IAdsConfigurations;
+import org.metatrans.apps.gravity.cfg.ads.AdsConfiguration_Gravity;
+import org.metatrans.commons.ads.api.IAdsProviders;
+import org.metatrans.commons.ads.impl.AdsConfigurations_DynamicImpl;
 import org.metatrans.commons.cfg.appstore.IAppStore;
 
 
@@ -11,16 +11,26 @@ public class Application_2DGravity extends Application_Gravity {
 
 
 	@Override
-	public IAppStore getAppStore() {
-		return IAppStore.OBJ_FDROID_OFFICIAL;
+	public void onCreate() {
+
+		super.onCreate();
+
+		getAdsConfigurations().addProviderConfig(
+				IAdsProviders.ID_HOME_ADS,
+				new AdsConfiguration_Gravity()
+		);
 	}
 
 
-	protected IAdsConfigurations adsConfigurations 	= new AdsConfigurations_Gravity(this);
+	@Override
+	public AdsConfigurations_DynamicImpl getAdsConfigurations() {
+
+		return (AdsConfigurations_DynamicImpl) super.getAdsConfigurations();
+	}
 
 
 	@Override
-	public IAdsConfigurations getAdsConfigurations() {
-		return adsConfigurations;
+	public IAppStore getAppStore() {
+		return IAppStore.OBJ_FDROID_OFFICIAL;
 	}
 }
---
+++ app/src/main/java/org/metatrans/apps/gravity/cfg/ads/AdsConfiguration_Gravity.java
@@ -0,0 +1,45 @@
+package org.metatrans.apps.gravity.cfg.ads;
+
+
+import org.metatrans.commons.ads.api.IAdsConfiguration;
+
+
+public class AdsConfiguration_Gravity implements IAdsConfiguration {
+
+
+	private static final String CONTAINER_CLASS_FDROID_ADS = "org.metatrans.commons.ads.impl.providers.home_ads.AdsContainer_HomeAds";
+
+
+	public AdsConfiguration_Gravity() {
+
+	}
+
+
+	@Override
+	public String getContainerClass() {
+
+		return CONTAINER_CLASS_FDROID_ADS;
+	}
+
+
+	@Override
+	public String[] getUnitIDs_Banners() {
+
+		return new String[] {"FIXED_STRING_getUnitIDs_Banners"};
+	}
+
+
+	@Override
+	public String[] getUnitIDs_Interstitial() {
+
+		return new String[] {"FIXED_STRING_getUnitIDs_Interstitial"};
+	}
+
+
+	@Override
+	public String getUnitID(String adID) {
+
+		throw new UnsupportedOperationException();
+	}
+}
+
